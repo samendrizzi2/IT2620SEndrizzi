ee635b971f165f317900de364665bc32c0598d0f
samendrizzi2 <s01161714@acad.tri-c.edu>
2017-02-27 18:49:05 -0500

    Add files via upload

---
+++ Lab06/Lab06DistanceCalculator/Lab06DistanceCalculator/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Dim Input1 As Integer
+    Dim Input2 As Integer
+    Dim A As Integer
+    Dim B As Integer
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Input1 = InputBox("Enter vehicle speed in miles per hour.", "Vehicle Speed")
+        Input2 = InputBox("Enter time traveled in hours.", "Time Traveled")
+        A = 1
+        B = 0
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add("Vehicle Speed: " & Input1 & " MPH")
+        ListBox1.Items.Add("Time Traveled: " & Input2 & " hours")
+        ListBox1.Items.Add("Hours    Distance Traveled")
+        ListBox1.Items.Add("__________________________")
+        Do Until A = Input2 + 1
+            B = A * Input1
+            ListBox1.Items.Add(A & "           " & B)
+            A += 1
+        Loop
+    End Sub
+End Class
---
+++ Lab06/Lab06SumNumbers/Lab06SumNumbers/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Dim Input As Integer
+    Dim Output As Integer
+    Dim A As Integer
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Input = 0
+        Output = 0
+        A = 0
+        Input = InputBox("Enter a positive integer value.", "Input Needed", 10)
+        If Input < 0 Then
+            MessageBox.Show("Negative Input", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            Do Until A = Input
+                Output += A + 1
+                A += 1
+            Loop
+            MessageBox.Show("The sum of the numbers 1 through " & Input & " is " & Output,
+            "Sum of Numbers", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+End Class
