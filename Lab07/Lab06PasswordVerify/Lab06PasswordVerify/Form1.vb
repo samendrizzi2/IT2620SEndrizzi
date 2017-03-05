ef4b128a53260f74c861613140858a8b87aa7c0d
samendrizzi2 <s01161714@acad.tri-c.edu>
2017-03-05 12:12:29 -0500

    Add files via upload

---
+++ Lab07/Lab06PasswordVerify/Lab06PasswordVerify/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Dim Flag As Boolean
+    Dim Regex As String
+    Private Sub IsValid(A As String)
+        Flag = False
+        If System.Text.RegularExpressions.Regex.Match(TextBox1.Text, "\d").Success Then
+            If System.Text.RegularExpressions.Regex.Match(TextBox1.Text, "[a-zA-Z]").Success Then
+                If TextBox1.Text.Length >= 6 Then
+                Else
+                    Flag = True
+                    MessageBox.Show("Password Must Contain At Least 6 Characters", "Error", MessageBoxButtons.OK)
+                End If
+            Else
+                Flag = True
+                MessageBox.Show("Password Must Contain Letters", "Error", MessageBoxButtons.OK)
+            End If
+        Else
+            Flag = True
+            MessageBox.Show("Password Must Contain Numbers", "Error", MessageBoxButtons.OK)
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        IsValid(TextBox1.Text)
+        If Flag = True Then
+        Else
+            MessageBox.Show("Password Accepted", "Success!", MessageBoxButtons.OK)
+        End If
+    End Sub
+End Class
---
+++ Lab07/Lab06RetailCalculator/Lab06RetailCalculator/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Dim a As Decimal
+    Dim b As Decimal
+    Private Sub CalculateRetail()
+        a = Convert.ToDecimal(TextBox1.Text)
+        b = Convert.ToDecimal(TextBox2.Text) / 100
+        TextBox3.Text = a * b + a
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CalculateRetail()
+    End Sub
+End Class
